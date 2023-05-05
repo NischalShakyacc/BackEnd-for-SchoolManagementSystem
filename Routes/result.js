eca7c59ccae3c7798ca770be06d434789dd63835
NischalShakyacc <95970505+NischalShakyacc@users.noreply.github.com>
2023-05-05 19:45:11 +0545

    After Delete

--- Routes/result.js
+++ Routes/result.js
@@ -11,7 +11,7 @@ const router  = express.Router();
 -----------------
 
 ROUTE 1 : Get all results 
-GET '/api/auth/getuser'. 
+GET '/api/result/getuser'. 
 * user must be logged in
 
 -------------------
@@ -20,11 +20,11 @@ GET '/api/auth/getuser'.
 router.get('/fetchresult', fetchuser, async (req,res)=>{
     try {
         //search for all results associated with ine user
-        const results = await Result.find({user: req.user.id});
-        res.json([results]);
+        const result = await Result.find({user: req.user.id});
+        res.json(result);
     } catch (error) {
         console.log(error.message);
-        res.status(500).send("Internal Server error occured.")
+        res.status(500).send("Internal Server error occured." + error.message)
     }       
 })
 
@@ -33,7 +33,7 @@ router.get('/fetchresult', fetchuser, async (req,res)=>{
 -----------------
 
 ROUTE 2 : Add a new result  
-GET '/api/auth/addresult'. 
+GET '/api/result/addresult'. 
 * user must be logged in
 
 -------------------
@@ -55,15 +55,105 @@ async (req,res)=>{
 
 
         //creata an object to be inserted to database
-        const results = new Result({
-            user: req.user.id,
+        const result = new Result({
             name,
-            userresult
+            userresult,
+            user: req.user.id,
         })
 
         //save data to database
-        const savedResult = await results.save();
+        const savedResult = await result.save();
         res.json(savedResult) 
+
+    } catch (error) {
+        console.log(error.message); 
+        res.status(500).send("Internal Server error occured.")
+    }
+    
+})
+
+
+/*
+-----------------
+
+ROUTE 3 :Update existing result 
+GET '/api/result/updateresult'. 
+* user must be logged in
+-------------------
+*/
+router.put('/updateresult/:id',fetchuser, [
+    body('name','Enter a valid name.').isLength({min:5}),
+    body('userresult','Enter a valid result.').isLength({min:5}) 
+],
+async (req,res)=>{
+    try {
+        //desctructer data from request
+        const {name, userresult} = req.body;
+
+        // If errors return bad request along with errors 
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors: errors.array()});
+        }
+
+        //creata an object to be inserted to database
+        const newResult = {};
+        if(name){newResult.name = name};
+        if(userresult){newResult.userresult = userresult};
+
+        //Find result to be Updated
+        let result = await Result.findById(req.params.id);
+        if(!result){
+            return res.status(404).send("Not Found.")
+        }
+
+        //Alllow Update if user does own
+        if(result.user.toString() !==  req.user.id){
+            return res.status(401).send("Not Allowed.")
+        }
+
+        result = await Result.findOneAndUpdate(req.params.id,{$set: newResult},{new:true}); 
+
+        res.json({result}) 
+
+    } catch (error) {
+        console.log(error.message); 
+        res.status(500).send("Internal Server error occured.")
+    }
+    
+})
+
+
+/*
+-----------------
+
+ROUTE 4  : Delete existing result 
+GET '/api/result/deletresult'. 
+* user must be logged in
+-------------------
+*/
+router.delete('/deleteresult/:id',fetchuser, [
+    body('name','Enter a valid name.').isLength({min:5}),
+    body('userresult','Enter a valid result.').isLength({min:5}) 
+],
+async (req,res)=>{
+    try {
+        //desctructer data from request
+        const {name, userresult} = req.body;
+
+        //Find result to be Deleted
+        let result = await Result.findById(req.params.id);
+        if(!result){
+            return res.status(404).send("Not Found.")
+        }
+
+         //Alllow delete if user does own
+        if(result.user.toString() !==  req.user.id){
+            return res.status(401).send("Not Allowed.")
+        }
+
+        result = await Result.findByIdAndDelete(req.params.id)
+        res.json({"Success":"Result Deleted",result:result})  
 
     } catch (error) {
         console.log(error.message); 
